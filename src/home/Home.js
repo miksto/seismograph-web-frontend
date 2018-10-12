6464031c9052f1bedd919e6dbc856d9cacb86928
Mikael Stockman <mikael.stockman@slagkryssaren.com>
2018-10-12 23:23:49 +0900

    Switch to svgz

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,19 +6,20 @@ class Home extends Component {
   render() {
     let yesterday = moment.utc().subtract(1, 'days').date()
     let previous_hour = moment.utc().subtract(1, 'hours').hour()
+    let image_host = 'http://46.101.184.224:8080'
 
     return (
       <div>
         <h2>Last 20 seconds</h2>
         <Graph/>
         <h2>Last 10 minutes</h2>
-        <img src="http://46.101.184.224:8080/images/latest.svg" />
+        <img src={`${image_host}/images/latest.svgz`} />
 
         <h2>Last hour</h2>
-        <img src={"http://46.101.184.224:8080/images/hour_" + previous_hour + ".svg"} />
+        <img src={`${image_host}/images/hour_${previous_hour}.svgz`} />
 
         <h2>Yesterday</h2>
-        <img src={"http://46.101.184.224:8080/images/day_" + yesterday + ".svg"} />
+        <img src={`${image_host}/images/day_${yesterday}.svgz`} />
       </div>
     );
   }
--- src/hourly/Hourly.js
+++ src/hourly/Hourly.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class Hourly extends Component {
   render() {
+    let image_host = 'http://46.101.184.224:8080'
     return (
       <div>
         <h2>Hourly plots</h2>
         {
           [...Array(24).keys()].map(
-            (object, i) => <img src={"http://46.101.184.224:8080/images/hour_" + i + ".svg"} />
+            (object, i) => <img src={`${image_host}/images/hour_${i}.svgz`} />
           )
         }
       </div>
